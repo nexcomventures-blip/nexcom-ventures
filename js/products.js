adddcba61df6b4f62710327e56e7dfc62b4d0886
nexcomventures-blip <nexcomventures@gmail.com>
2026-05-19 09:01:43 +0300

    Daily rotation of featured laptops

--- js/products.js
+++ js/products.js
@@ -1,26 +1,6 @@
 const ALL_PRODUCTS = [
   {
     "id": "feat-1",
-    "brand": "HP",
-    "name": "HP EliteBook 840 G8 i7",
-    "specs": "Intel Core i7 11th Gen \u2022 16GB RAM \u2022 512GB SSD \u2022 14\" FHD",
-    "price": 63000,
-    "category": "exuk business hp featured",
-    "badge": "hot",
-    "img": "https://www.hp.com/ca-en/shop/Html/Merch/Images/c07010411_1750x1285.jpg"
-  },
-  {
-    "id": "feat-2",
-    "brand": "Apple",
-    "name": "MacBook Air 13 M2 (2022)",
-    "specs": "Apple M2 Chip \u2022 8GB RAM \u2022 256GB SSD \u2022 Liquid Retina",
-    "price": 115000,
-    "category": "exuk apple macbook featured",
-    "badge": "new",
-    "img": "https://www.apple.com/v/macbook-air-m2/c/images/overview/hero/hero_static__f9clv6u74u6u_large.jpg"
-  },
-  {
-    "id": "feat-3",
     "brand": "Lenovo",
     "name": "Lenovo X1 Carbon I7 8Th 16/512Gb.",
     "specs": "x1 carbon i7 8th 16/512gb.",
@@ -30,12 +10,52 @@ const ALL_PRODUCTS = [
     "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Lenovo+x1+carbon+i7+8th"
   },
   {
+    "id": "feat-2",
+    "brand": "Dell",
+    "name": "Dell Xps 15 9570 I7 8Th 16/512Gb 4Gb Graphics",
+    "specs": "xps 15 9570 i7 8th 16/512gb 4gb Graphics",
+    "price": 50000,
+    "category": "exuk business dell featured",
+    "badge": "exuk",
+    "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Dell+xps+15+9570+i7"
+  },
+  {
+    "id": "feat-3",
+    "brand": "Apple",
+    "name": "MacBook Air 13 M2 (2022)",
+    "specs": "Apple M2 Chip \u2022 8GB RAM \u2022 256GB SSD \u2022 Liquid Retina",
+    "price": 115000,
+    "category": "exuk apple macbook featured",
+    "badge": "new",
+    "img": "https://www.apple.com/v/macbook-air-m2/c/images/overview/hero/hero_static__f9clv6u74u6u_large.jpg"
+  },
+  {
     "id": "feat-4",
+    "brand": "Dell",
+    "name": "Dell Xps 13 9305 I7 11Th 16/512Gb",
+    "specs": "xps 13 9305 i7 11th 16/512gb",
+    "price": 60000,
+    "category": "exuk business dell featured",
+    "badge": "exuk",
+    "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Dell+xps+13+9305+i7"
+  },
+  {
+    "id": "tmp-6570",
+    "brand": "HP",
+    "name": "HP EliteBook 840 G8 i7",
+    "specs": "Intel Core i7 11th Gen \u2022 16GB RAM \u2022 512GB SSD \u2022 14\" FHD",
+    "price": 63000,
+    "category": "exuk business hp",
+    "badge": "hot",
+    "img": "https://www.hp.com/ca-en/shop/Html/Merch/Images/c07010411_1750x1285.jpg"
+  },
+  {
+    "id": "tmp-8284",
     "brand": "Lenovo",
     "name": "Lenovo X390 I5 8Th 8/256Gb",
     "specs": "x390 i5 8th 8/256gb",
     "price": 22000,
-    "category": "exuk business lenovo featured",
+    "category": "exuk business lenovo",
     "badge": "exuk",
     "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Lenovo+x390+i5+8th+8/256gb"
   },
@@ -48,26 +68,6 @@ const ALL_PRODUCTS = [
     "category": "exuk business lenovo",
     "badge": "exuk",
     "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Lenovo+x13+i7+10th+16/512gb"
-  },
-  {
-    "id": "tmp-9498",
-    "brand": "Dell",
-    "name": "Dell Xps 15 9570 I7 8Th 16/512Gb 4Gb Graphics",
-    "specs": "xps 15 9570 i7 8th 16/512gb 4gb Graphics",
-    "price": 50000,
-    "category": "exuk business dell",
-    "badge": "exuk",
-    "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Dell+xps+15+9570+i7"
-  },
-  {
-    "id": "tmp-2119",
-    "brand": "Dell",
-    "name": "Dell Xps 13 9305 I7 11Th 16/512Gb",
-    "specs": "xps 13 9305 i7 11th 16/512gb",
-    "price": 60000,
-    "category": "exuk business dell",
-    "badge": "exuk",
-    "img": "https://via.placeholder.com/300x200/0a0a0a/f01a24?text=Dell+xps+13+9305+i7"
   },
   {
     "id": "tmp-6898",
